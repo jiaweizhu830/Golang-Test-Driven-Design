c2ada09a22d8391482a99ac2a0d546e9ba86df79
Jiawei Zhu <jzhu@MacBook-Pro-2.attlocal.net>
2021-09-25 23:33:59 -0700

    basic greeting

--- src/hello_test.go
+++ src/hello_test.go
@@ -3,28 +3,35 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	// declaring and re-use values
-	got := Hello("world")
-	want := "Hello, world"
 
-	if got != want {
-		// print out a message and fail the test
-		// f stands for format => so that we can use %q (placeholder)
-		t.Errorf("got %q want %q", got, want)
+	// assign functions to variables
+	assertCorrectMessage := func(t testing.TB, got, want string) {
+		// to tell the test suite that this method is a helper
+		t.Helper()
+		
+		if got != want {
+			// print out a message and fail the test
+			// f stands for format => so that we can use %q (placeholder)
+			t.Errorf("got %q want %q", got, want)
+		}
 	}
+
+	// subtests
+	t.Run("saying hello to people", func(t *testing.T) {
+		// declaring and re-use values
+		got := Hello("Chris")
+		want := "Hello, Chris"
+
+		assertCorrectMessage(t, got, want);
+	})
+
+	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
+		// declaring and re-use values
+		got := Hello("")
+		want := "Hello, World"
+
+		assertCorrectMessage(t, got, want);
+	})
  }
-
- func TestHelloGreet(t *testing.T) {
-	// declaring and re-use values
-	got := Hello("Chris")
-	want := "Hello, Chris"
-
-	if got != want {
-		// print out a message and fail the test
-		// f stands for format => so that we can use %q (placeholder)
-		t.Errorf("got %q want %q", got, want)
-	}
- }
-
 
  // Go is statically typed
